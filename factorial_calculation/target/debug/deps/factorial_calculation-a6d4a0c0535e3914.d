162d8e5348d3d6fecab326aa8aa50eea0d4ebf0b
Ali H. <ahahswe@gmail.com>
2024-02-13 23:08:14 +0200

    .gitignore modified

---
+++ factorial_calculation/target/debug/deps/factorial_calculation-a6d4a0c0535e3914.d
@@ -0,0 +1,5 @@
+C:\Users\alims\Documents\Rust\Rust\factorial_calculation\target\debug\deps\libfactorial_calculation-a6d4a0c0535e3914.rmeta: src\main.rs
+
+C:\Users\alims\Documents\Rust\Rust\factorial_calculation\target\debug\deps\factorial_calculation-a6d4a0c0535e3914.d: src\main.rs
+
+src\main.rs:
