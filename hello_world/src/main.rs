e3cec0c3695f4ea1002c36b31088cdb050e573a5
Ali H. <ahahswe@gmail.com>
2024-02-13 14:10:51 +0200

    Initial commit

---
+++ hello_world/src/main.rs
@@ -0,0 +1,6 @@
+// Begin with the main function fn main() and an opening curly brace
+fn main() {
+    // Print a message to the standard out enclosed with parentheses and ended with a semicolon
+    println!("Hello, World!");
+    // End the main function
+}
