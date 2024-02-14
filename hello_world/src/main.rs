25f53536a8b5c596ae9286c5eea70c2f50e3555e
Ali H. <ahahswe@gmail.com>
2024-02-14 04:03:04 +0200

    main.rs updated

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,6 +1,6 @@
 // Begin with the main function fn main() and an opening curly brace
 fn main() {
     // Print a message to the standard out enclosed with parentheses and ended with a semicolon
-    println!("Hello, World!");
+    println!("Hello, Rust!");
     // End the main function
 }
