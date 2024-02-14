6672e72e4a6fa1124da0c548ddd8ef97b16485ee
Ali H. <ahahswe@gmail.com>
2024-02-14 06:50:12 +0200

    ignored

--- hello_world/target/doc/search-index.js
+++
@@ -1,5 +0,0 @@
-var searchIndex = new Map(JSON.parse('[\
-["hello_world",{"doc":"","t":"H","n":["main"],"q":[[0,"hello_world"]],"d":[""],"i":[0],"f":[[[],1]],"c":[],"p":[[1,"tuple"]],"b":[]}]\
-]'));
-if (typeof exports !== 'undefined') exports.searchIndex = searchIndex;
-else if (window.initSearch) window.initSearch(searchIndex);
--- hello_world/target/doc/src-files.js
+++
@@ -1,4 +0,0 @@
-var srcIndex = new Map(JSON.parse('[\
-["hello_world",["",[],["main.rs"]]]\
-]'));
-createSrcSidebar();
