50ca8fb0f647418a5d252573ac38f662225a33f8
Ali H. <ahahswe@gmail.com>
2024-02-14 04:25:24 +0200

    .gitignore deleted

---
+++ hello_world/target/debug/deps/hello_world-ebab8ff2a6a09bec.d
@@ -0,0 +1,5 @@
+C:\Users\alims\Documents\Rust\Rust\hello_world\target\debug\deps\hello_world-ebab8ff2a6a09bec.exe: src\main.rs
+
+C:\Users\alims\Documents\Rust\Rust\hello_world\target\debug\deps\hello_world-ebab8ff2a6a09bec.d: src\main.rs
+
+src\main.rs:
